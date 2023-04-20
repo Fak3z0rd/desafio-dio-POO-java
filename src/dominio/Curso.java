6fe30d67688f78a0ad4c268d54f5d8276a2229d4
f4k3z0rd <vinicius.meireles@outlook.com>
2023-04-20 17:33:02 -0300

    Criação da classe abstrata Conteudo Curso e Mentoria herdam de Conteudo Define o cálculo de xp com a função calcularXp

--- src/dominio/Curso.java
+++ src/dominio/Curso.java
@@ -1,43 +1,27 @@
 package dominio;
 
-public class Curso {
-    String titulo;
-    String descricao;
-    int cargaHoraria;
+public class Curso extends Conteudo {
 
+    private int cargaHoraria;
+    
     public int getCargaHoraria() {
         return cargaHoraria;
     }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
+    
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    Double calcularXp () {
-        return 0d;
+    @Override
+    public Double calcularXp() {
+        return XP_PADRAO + cargaHoraria;
     };
-
+    
     @Override
     public String toString() {
         return "Mentoria{ " +
-                "titulo= " + titulo + 
-                ", descrição= " + descricao +
+                "titulo= " + getTitulo() + 
+                ", descrição= " + getDescricao() +
                 ", carga horária= " + cargaHoraria +
                 "}";
     }
--- src/dominio/Mentoria.java
+++ src/dominio/Mentoria.java
@@ -2,44 +2,28 @@ package dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    String titulo;
-    String descricao;
+public class Mentoria extends Conteudo {
+
     LocalDate dataMentoria;
 
     public LocalDate getDataMentoria() {
         return dataMentoria;
     }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
     public void setDataMentoria(LocalDate dataMentoria) {
         this.dataMentoria = dataMentoria;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    Double calcularXp () {
-        return 0d;
+    @Override
+    public Double calcularXp () {
+        return XP_PADRAO + 10d;
     };
 
     @Override
     public String toString() {
         return "Mentoria{ " +
-                "titulo= " + titulo + 
-                ", descrição= " + descricao +
+                "titulo= " + getTitulo() + 
+                ", descrição= " + getDescricao() +
                 ", data= " + dataMentoria +
                 "}";
     }
