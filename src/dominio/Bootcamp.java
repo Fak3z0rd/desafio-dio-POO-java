2e3b0fab98dc672f79a4904053da5a9c0aa966d0
f4k3z0rd <vinicius.meireles@outlook.com>
2023-04-20 17:59:31 -0300

    Cria classe Dev Modifica classe Bootcamp

--- src/dominio/Bootcamp.java
+++ src/dominio/Bootcamp.java
@@ -1,54 +1,76 @@
 package dominio;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 public class Bootcamp {
     private String nome;
     private String descricao;
-    private LocalDate dataInicio;
-    private LocalDate dataFim;
+    private final LocalDate dataInicio = LocalDate.now();
+    private final LocalDate dataFim = dataInicio.plusDays(45);
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
 
-
-    public LocalDate getDataFim() {
-        return dataFim;
-    }
-
-    public LocalDate getDataInicio() {
-        return dataInicio;
+    public String getNome() {
+        return nome;
     }
 
     public String getDescricao() {
         return descricao;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setDataFim(LocalDate dataFim) {
-        this.dataFim = dataFim;
-    }
-
-    public void setDataInicio(LocalDate dataInicio) {
-        this.dataInicio = dataInicio;
+    
+    public LocalDate getDataFim() {
+        return dataFim;
     }
     
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+    
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }    
+
     @Override
-    public String toString() {
-        return "Mentoria{ " +
-                "nome= " + nome + 
-                ", descrição= " + descricao +
-                ", data início= " + dataInicio +
-                ", data fim= " + dataFim +
-                "}";
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(o, bootcamp)  
+        && Objects.equals(descricao, bootcamp.descricao) 
+        && Objects.equals(dataInicio, bootcamp.dataInicio)
+        && Objects.equals(dataFim, bootcamp.dataFim)
+        && Objects.equals(devsInscritos, bootcamp.devsInscritos)
+        && Objects.equals(conteudos, bootcamp.conteudos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataInicio, dataFim, devsInscritos, conteudos);
     }
     
 
--- src/dominio/Dev.java
+++ src/dominio/Dev.java
@@ -1,5 +1,44 @@
 package dominio;
 
-public class Dev {
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {}
+
+    public void progredir() {}
+
+    public void calcularTotalXp() {}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(o, dev)  
+        && Objects.equals(conteudosInscritos, dev.conteudosInscritos) 
+        && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
 }
